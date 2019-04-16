09d13fa5cfbe4fc24e69d338f208ac50e9ca28ca
Oscar Key <oscar.c.key@gmail.com>
2019-04-17 09:17:36 +1200

    fix cmake skip licence spelling

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -18,7 +18,7 @@ if [ ! -e $CMAKE_VERSION_NUM ] ; then
     CMAKE_STR="$(awk -F'.' '{ print $1"."$2"."$3 }' <<< ${CMAKE_VERSION_NUM}.0)"
     echo "RUN wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_STR}/cmake-${CMAKE_STR}-Linux-x86_64.sh && \
     chmod u+x cmake-${CMAKE_STR}-Linux-x86_64.sh && \
-    ./cmake-${CMAKE_STR}-Linux-x86_64.sh --skip-licence && \
+    ./cmake-${CMAKE_STR}-Linux-x86_64.sh --skip-license && \
     cmake --version"
 fi
 
